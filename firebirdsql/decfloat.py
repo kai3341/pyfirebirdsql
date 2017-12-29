48607e13ae79fd102d0d49384add473de0c36754
Hajime Nakagami <nakagami@gmail.com>
2017-12-29 15:03:31 +0900

    wip

--- firebirdsql/decfloat.py
+++ firebirdsql/decfloat.py
@@ -32,17 +32,25 @@ def from_decimal128(d):
 
 def to_decimal128(b):
     "decimal 128 bytes to Decimal"
+    print()
+    for c in b:
+        print(bin(byte_to_int(c)), end=' ')
+    print()
+
     sign = 1 if (byte_to_int(b[0]) & 0x80) else 0
-    combination = ((byte_to_int(b[0]) & 0x7f) << 10) + (byte_to_int(b[1]) << 2) + (byte_to_int(b[2]) >> 6)
+    combination = (byte_to_int(b[0]) & 0x7F) * 1024 + byte_to_int(b[1]) * 4 + byte_to_int(b[2]) // 64
 
     if (b[0] & 0x60) == 0x60:
         exponent = ((b[0] & 0x1f) * 256 + b[1]) * 2 - 6176
     else:
         exponent = ((b[0] & 0x7f) * 256 + b[1]) // 2 - 6176
+    if combination & 0b01000000000000000:
+        print(combination)
+        print('A', combination & 0b00111111111111111)
 
     digits = srp.bytes2long(b) & 0b11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111
 
-    print('sign<%s>combination<%s>digits<%s>' %  (bin(sign), bin(combination), bin(digits)))
+    print('sign<%s>combination<%s>digits<%s>' %  (bin(sign), bin(combination), hex(digits)))
     v = {
         (0, 0, 8160): Decimal('NaN'),
         (1, 0, 8160): Decimal('-NaN'),
